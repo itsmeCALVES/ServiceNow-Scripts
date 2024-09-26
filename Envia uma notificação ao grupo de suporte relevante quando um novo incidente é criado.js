e1b2ade0134918a234da5c5a417bdf3d795a455d
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 04:55:46 -0300

    add BS Envia uma notificação ao grupo de suporte relevante quando um novo incidente é criado

---
+++ Envia uma notificação ao grupo de suporte relevante quando um novo incidente é criado.js
@@ -0,0 +1,4 @@
+// Envia uma notificação ao grupo de suporte relevante quando um novo incidente é criado
+(function executeRule(current, previous /*null when async*/) {
+    gs.eventQueue('incident.created', current, current.assignment_group);
+})(current, previous);

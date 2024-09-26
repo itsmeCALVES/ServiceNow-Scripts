fc9f2eaff3a6b632bb7c87fbe2b7fd405908330a
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 04:53:07 -0300

    add BS Define o campo 'assigned_to' automaticamente com base no grupo de atribuição

---
+++ Define o campo 'assigned_to' automaticamente com base no grupo de atribuição.js
@@ -0,0 +1,6 @@
+// Define o campo 'assigned_to' automaticamente com base no grupo de atribuição
+(function executeRule(current, previous /*null when async*/) {
+    if (current.assignment_group == 'Hardware Support') {
+        current.assigned_to = 'tech_support_user';
+    }
+})(current, previous);

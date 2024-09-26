3c122d61452011a19e5cb722b18639f47f82a611
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 04:50:48 -0300

    add BS Adiciona automaticamente o tempo de resposta (SLA) com base na prioridade do incidente

---
+++ Adiciona automaticamente o tempo de resposta (SLA) com base na prioridade do incidente.js
@@ -0,0 +1,8 @@
+// Adiciona automaticamente o tempo de resposta (SLA) com base na prioridade do incidente
+(function executeRule(current, previous /*null when async*/) {
+    if (current.priority == '1') {
+        current.response_due = gs.daysAgoStart(1); // Tempo de resposta de 1 dia para prioridade 1
+    } else if (current.priority == '2') {
+        current.response_due = gs.daysAgoStart(3); // Tempo de resposta de 3 dias para prioridade 2
+    }
+})(current, previous);

f738ef0d7cb95d24139c2d84840d9164b491b048
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 04:51:40 -0300

    add BS Cria automaticamente uma tarefa de acompanhamento quando o incidente for reaberto

---
+++ Cria automaticamente uma tarefa de acompanhamento quando o incidente for reaberto.js
@@ -0,0 +1,9 @@
+// Cria automaticamente uma tarefa de acompanhamento quando o incidente for reaberto
+(function executeRule(current, previous /*null when async*/) {
+    if (current.state == 'Reopened') {
+        var task = new GlideRecord('task');
+        task.initialize();
+        task.short_description = 'Tarefa relacionada ao incidente reaberto';
+        task.insert();
+    }
+})(current, previous);

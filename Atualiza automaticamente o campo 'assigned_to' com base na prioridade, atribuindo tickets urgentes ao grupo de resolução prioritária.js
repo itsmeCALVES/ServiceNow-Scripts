23668e5d274f84204ab2edcb2f8281c4d19dc324
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 03:53:44 -0300

    add CS Atualiza automaticamente o campo 'assigned_to' com base na prioridade, atribuindo tickets urgentes ao grupo de resolução prioritária

---
+++ Atualiza automaticamente o campo 'assigned_to' com base na prioridade, atribuindo tickets urgentes ao grupo de resolução prioritária.js
@@ -0,0 +1,10 @@
+// Atualiza automaticamente o campo 'assigned_to' com base na prioridade, atribuindo tickets urgentes ao grupo de resolução prioritária
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+    if (isLoading || newValue == '') {
+        return;
+    }
+    
+    if (g_form.getValue('priority') == '1') {
+        g_form.setValue('assigned_to', 'priority_support_group');
+    }
+}

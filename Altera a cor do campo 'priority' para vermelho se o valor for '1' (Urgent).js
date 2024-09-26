66bef30d3b2b4fb39faef0f833b7277ba8ec94f9
itsmeCALVES <c.ale_sandro@live.com>
2024-09-26 03:28:48 -0300

    add CS Altera a cor do campo 'priority' para vermelho se o valor for '1' (Urgent)

---
+++ Altera a cor do campo 'priority' para vermelho se o valor for '1' (Urgent).js
@@ -0,0 +1,9 @@
+// Altera a cor do campo 'priority' para vermelho se o valor for '1' (Urgent)
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+    if (isLoading || newValue == '') {
+        return;
+    }
+    if (newValue == '1') {
+        g_form.setFieldStyle('priority', 'color', 'red');
+    }
+}
